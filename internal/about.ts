1d31eb51628f57efa1254231b18ff6dda19a8eac
gopher93185789 <102717@glr.nl>
2025-12-04 14:35:15 +0100

    feat: optimize home page rendering by caching HTML content

--- internal/about.ts
+++ internal/about.ts
@@ -1,9 +1,18 @@
 import * as fs from "fs";
 import * as path from "path";
 
+let homePageHtml: string = "";
+
 export async function RenderHomePage(): Promise<string> {
   try {
-    return fs.readFileSync(path.join(__dirname, "home.html"),"utf-8") as string;
+    if (homePageHtml === "") {
+      homePageHtml = fs.readFileSync(
+        path.join(__dirname, "home.html"),
+        "utf-8"
+      ) as string;
+    }
+
+    return homePageHtml;
   } catch (e: any) {
     return `<h1 class="text-white text-3xl h-screen w-full flex items-center justify-center">An internal server error occured</h1>`;
   }
